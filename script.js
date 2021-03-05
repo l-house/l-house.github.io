b10cb833193a62f6de5b300fe89d129723d9667b
Laylah House <laylahhouse@utweb-ha-z1-p03.its.utexas.edu>
2021-03-04 19:23:07 -0600

    finished website (minus pictures

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
-document.getElementById("aboutButton").addEventListener("click", changeContentTitle);
+/* document.getElementById("aboutButton").addEventListener("click", changeContentTitle);
 
 function changeContentTitle() {
   document.getElementById("contentTitle").innerHTML = "About Me";
+}
+
+var aboutIsClicked = false;
+var experienceIsClicked = false;
+var contactIsClicked = false;
+
+function 
+
+function toggleContent(){
+    var contentTitle = document.getElementById('contentTitle');
+
+    
+
+    var currentTitle = contentTitle.innerHTML;
+    console.log(currentTitle);
+}
+*/
+
+var aboutContent = "Hi my name is Laylah House I am currently a senior at the University of Texas at Austin.";
+
+function changeToHome(){
+    document.getElementById("contentTitle").innerHTML = "Welcome to my website!";
+    document.getElementById("contentText").innerHTML = "Click on one of the tabs to learn more about me!";
+}
+
+function changeToAbout(){
+    document.getElementById("contentTitle").innerHTML = "About Me";
+    document.getElementById("contentText").innerHTML = aboutContent;
+}
+
+function changeToExperience(){
+    document.getElementById("contentTitle").innerHTML = "Experience";
+    document.getElementById("contentText").innerHTML = "laylah + tommy";
+}
+
+function changeToContact(){
+    document.getElementById("contentTitle").innerHTML = "Contact";
+    document.getElementById("contentText").innerHTML = "laylah + brandon";
 }
